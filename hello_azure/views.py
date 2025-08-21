f290802544d6253af41e87e1d9c446443cc03efb
Dung Nguyen <Dung.Nguyen@optimizely.com>
2025-08-21 12:10:10 +0700

    Handle newhello request

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -20,3 +20,18 @@ def hello(request):
             return render(request, 'hello_azure/hello.html', context)
     else:
         return redirect('index')
+
+
+def newhello(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+
+        if name is None or name == '':
+            print("Request for hello page received with no name or blank name -- redirecting")
+            return redirect('index')
+        else:
+            print("Request for hello page received with name=%s" % name)
+            context = {'name': name}
+            return render(request, 'hello_azure/newhello.html', context)
+    else:
+        return redirect('index')
